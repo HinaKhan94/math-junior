c97346f6262fb6fcd1e37197ab8749ba73bbe428
HinaKhan94 <>
2023-08-13 20:41:59 +0000

    add code to decrement the score

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var flag_div = 0;
 var answer_add = 0;
 var answer_sub = 0;
 var answer_mul = 0;
+var answer_div = 0;
 let c_score = document.getElementById('correct');
 let ic_score = document.getElementById('incorrect');
 
@@ -21,15 +22,24 @@ let result2 = document.getElementById('option2');
 let result3 = document.getElementById('option3');
 let result4 = document.getElementById('option4');
 
+// Function for incrementing the score
+
 function scorecorrect(a) {
     score = a + 1;
     return score;
 }
+
+// Function for decrementing the score
+
 function scoreincorrect(a) {
     score = a + 1;
     return score;
 }
 
+/**
+ * game type: addition
+ * will only generate addition game equation when pressed
+ */
 button1.addEventListener('click', function () {
     flag_add = 1;
     flag_sub = 0;
@@ -42,6 +52,12 @@ button1.addEventListener('click', function () {
     displayOptions(answer_add);
 
 });
+
+/**
+ * game type: Subtraction
+ * will only generate subtraction game equation when pressed
+ */
+
 button2.addEventListener('click', function () {
     flag_add = 0;
     flag_sub = 1;
@@ -58,6 +74,12 @@ button2.addEventListener('click', function () {
     displaysubtract(operand1, operand2);
     displayOptions(answer_sub);
 });
+
+/**
+ * game type: Multiplication
+ * will only generate multiplication game equation when pressed
+ */
+
 button3.addEventListener('click', function () {
     flag_add = 0;
     flag_sub = 0;
@@ -70,6 +92,11 @@ button3.addEventListener('click', function () {
     displaymultiply(operand1, operand2);
     displayOptions(answer_mul);
 });
+
+/**
+ * game type: Division
+ * will only generate division game equation when pressed
+ */
 
 button4.addEventListener('click', function () {
     flag_add = 0;
@@ -89,8 +116,15 @@ button4.addEventListener('click', function () {
 
 });
 
+/**
+ * function for generating answer option 1 
+ * and then compares it with the correct answer depending on which game type is running (+,-,x,/)
+ * through while loops
+ */
+
 result1.addEventListener('click', function () {
     sAnswer = result1.textContent;
+    //while loop will execute only once and when addition game type addition is selected
     while (flag_add) {
         if (sAnswer == answer_add) {
             correct_value = scorecorrect(parseInt(c_score.textContent));
@@ -102,6 +136,7 @@ result1.addEventListener('click', function () {
             flag_add = 0;
         }
     }
+    //while loop will execute only once and when the game type subtraction is selected
     while (flag_sub) {
         if (sAnswer == answer_sub) {
             correct_value = scorecorrect(parseInt(c_score.textContent));
@@ -113,6 +148,7 @@ result1.addEventListener('click', function () {
             flag_sub = 0;
         }
     }
+    //while loop will execute only once and when the game type multiplication is selected
     while (flag_mul) {
         if (sAnswer == answer_mul) {
             correct_value = scorecorrect(parseInt(c_score.textContent));
@@ -124,6 +160,7 @@ result1.addEventListener('click', function () {
             flag_mul = 0;
         }
     }
+    //while loop will execute only once and when the game type division is selected
     while (flag_div) {
         if (sAnswer == answer_div) {
             correct_value = scorecorrect(parseInt(c_score.textContent));
@@ -137,6 +174,11 @@ result1.addEventListener('click', function () {
     }
 });
 
+/**
+ * function for generating answer option 2 
+ * and then compares it with the correct answer depending on which game type is running (+,-,x,/)
+ * through while loops
+ */
 result2.addEventListener('click', function () {
     sAnswer = result2.textContent;
     while (flag_add) {
@@ -185,6 +227,12 @@ result2.addEventListener('click', function () {
     }
 });
 
+/**
+ * function for generating answer option 3 
+ * and then compares it with the correct answer depending on which game type is running (+,-,x,/)
+ * through while loops
+ */
+
 result3.addEventListener('click', function () {
     sAnswer = result3.textContent;
     while (flag_add) {
@@ -232,6 +280,13 @@ result3.addEventListener('click', function () {
         }
     }
 });
+
+/**
+ * function for generating answer option 4 
+ * and then compares it with the correct answer depending on which game type is running (+,-,x,/)
+ * through while loops
+ */
+
 result4.addEventListener('click', function () {
     sAnswer = result4.textContent;
     while (flag_add) {
@@ -280,6 +335,7 @@ result4.addEventListener('click', function () {
     }
 
 });
+
 /**
  * Display game questioy by creating two random numbers each 
  * time according to the categories selected by the user
